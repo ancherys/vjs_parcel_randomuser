37a44c9ac00204a915903a30a828239e1403546c
ancherys <62359633+ancherys@users.noreply.github.com>
2020-07-20 22:59:59 +0100

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+	const template = `
+    <header>
+        <h1>My Parcel app</h1>
+        <p>This is a boilerplate for JS workflow using Parcel</p>
+    </header>
+    `;
+
+	return template;
+};
+
+export default Header;
